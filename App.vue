8eaa0d2dca2680b69c2372ed8cc09768f1185e7a
BLACKPINKL <1251316155@qq.com>
2020-12-07 22:44:28 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,116 @@
+<script>
+	import Vue from 'vue'
+	import configKey from './config/keys'
+	import configApp from './config/app'
+	export default {
+		onLaunch() {
+			// colorui 颜色列表
+			Vue.prototype.ColorList = this.getColorList()
+			// console.log(this);
+		},
+		onShow() {
+			console.log('App Show')
+		},
+		onHide() {
+			console.log('App Hide')
+		},
+		methods: {
+			getColorList() {
+				const colorList = [{
+						title: '嫣红',
+						name: 'red',
+						color: '#e54d42'
+					},
+					{
+						title: '桔橙',
+						name: 'orange',
+						color: '#f37b1d'
+					},
+					{
+						title: '明黄',
+						name: 'yellow',
+						color: '#fbbd08'
+					},
+					{
+						title: '橄榄',
+						name: 'olive',
+						color: '#8dc63f'
+					},
+					{
+						title: '森绿',
+						name: 'green',
+						color: '#39b54a'
+					},
+					{
+						title: '天青',
+						name: 'cyan',
+						color: '#1cbbb4'
+					},
+					{
+						title: '海蓝',
+						name: 'blue',
+						color: '#0081ff'
+					},
+					{
+						title: '姹紫',
+						name: 'purple',
+						color: '#6739b6'
+					},
+					{
+						title: '木槿',
+						name: 'mauve',
+						color: '#9c26b0'
+					},
+					{
+						title: '桃粉',
+						name: 'pink',
+						color: '#e03997'
+					},
+					{
+						title: '棕褐',
+						name: 'brown',
+						color: '#a5673f'
+					},
+					{
+						title: '玄灰',
+						name: 'grey',
+						color: '#8799a3'
+					},
+					{
+						title: '草灰',
+						name: 'gray',
+						color: '#aaaaaa'
+					},
+					{
+						title: '墨黑',
+						name: 'black',
+						color: '#333333'
+					},
+					{
+						title: '雅白',
+						name: 'white',
+						color: '#ffffff'
+				}]
+				
+
+				return colorList
+			},
+			// 获取app常量
+			getConfigKey(key = '') {
+				key = key.toUpperCase()
+				return configKey[key]
+			},
+			// 获取系统配置
+			getConfigApp(key) {
+				return configApp[key]
+			}
+		},
+		
+
+	}
+</script>
+
+<style lang="scss">
+	@import "colorui/main.css";
+	@import "colorui/icon.css";
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <view class="login">
+    <view class="bg-login-box">
+      <image mode="" class="bg-login-img" src="/static/login/bg-login.jpg"></image>
+    </view>
+
+    <!-- TODO: 登录 注册表单   -->
+
+
+    <view class="login-btns text-center width-max">
+      <button @click="handleClickBtn($event, 'login')" class="cu-btn lg bg-theme margin-bottom-lg color-title">
+        登录<text class="icon cuIcon-right color-title"></text>
+      </button>
+      <button @click="handleClickBtn($event, 'reg')" class="cu-btn lg bg-icon color-title">
+        注册<text class="icon cuIcon-right color-title"></text>
+      </button>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    handleClickBtn(e, type) {
+
+    }    
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+  .bg-login-box {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 0;
+    .bg-login-img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+  }
+
+  .login-btns {
+    position: absolute;
+    bottom: 200rpx;
+    left: 0;
+    
+    .cu-btn {
+      width: 70%;
+      opacity: 0.7;
+      .icon {
+        position: absolute;
+        right: 30rpx;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }
+
+    
+  }
+}
+</style>
